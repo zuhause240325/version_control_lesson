e7b3648590904cee4878681054bd9f4569716e50
zuhause240325 <zuhause240325@gmail.com>
2026-06-05 19:11:28 +0300

    changes to analysis.py, comments added

--- analytics.py
+++ analytics.py
@@ -1,4 +1,7 @@
+#import libraries
 import pandas as pd
 import numpy as np
+#create DataFrame from dictionary
 df=pd.DataFrame({'Name':['Olga','Maks','Slava'], 'Age':[22,34,45]})
-df.head()
+#print df head
+print(df.head())
